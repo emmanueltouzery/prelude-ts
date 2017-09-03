a23a7db9d2f02a751ab911fecd0eb74c92976e80
Emmanuel Touzery <etouzery@gmail.com>
2017-09-03 19:14:41 +0200

    export tuple2

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from "./Option";
 export * from "./Vector";
 export * from "./HashMap";
 export * from "./HashSet";
+export * from "./Tuple2";
 export * from "./Value";
 export * from "./Comparison";
