a95996088032c2de31ea151fe0be31b7a73c70c7
Emmanuel Touzery <etouzery@gmail.com>
2017-09-08 20:56:49 +0200

    tuples now can be structs too. add a couple of useful methods for tuples.

--- src/Tuple2.ts
+++ src/Tuple2.ts
@@ -10,28 +10,92 @@ import { WithEquality, areEqual, getHashCode } from "./Comparison";
  */
 export class Tuple2<T,U> implements Value {
     
-    private constructor(private _fst: T & WithEquality,
-                        private _snd: U & WithEquality) {}
+    private constructor(private _fst: T,
+                        private _snd: U) {}
 
     /**
      * Build a pair of value from both values.
+     * Equality requirements.
      */
     static of<T,U>(fst: T & WithEquality, snd: U & WithEquality) {
         return new Tuple2(fst,snd);
     }
 
     /**
+     * Build a pair of value from both values.
+     * No equality requirements.
+     */
+    static ofStruct<T,U>(fst: T, snd: U) {
+        return new Tuple2(fst,snd);
+    }
+
+    /**
+     * Build a tuple2 from javascript pair.
+     * Equality requirements.
+     */
+    static ofArray<T,U>(pair: [T & WithEquality, U & WithEquality]): Tuple2<T,U> {
+        return new Tuple2(pair[0], pair[1]);
+    }
+
+    /**
+     * Build a tuple2 from javascript pair.
+     * No equality requirements.
+     */
+    static ofArrayStruct<T,U>(pair: [T, U]): Tuple2<T,U> {
+        return new Tuple2(pair[0], pair[1]);
+    }
+
+    /**
      * Extract the first value from the pair
      */
-    fst(): T & WithEquality {
+    fst(): T {
         return this._fst;
     }
 
     /**
      * Extract the second value from the pair
      */
-    snd(): U & WithEquality {
+    snd(): U {
         return this._snd;
+    }
+
+    /**
+     * Maps the first component of this tuple to a new value.
+     * Equality requirements.
+     */
+    map1<V>(fn: (v:T)=>V&WithEquality): Tuple2<V,U> {
+        return new Tuple2(fn(this._fst), this._snd);
+    }
+
+    /**
+     * Maps the first component of this tuple to a new value.
+     * No equality requirements.
+     */
+    map1Struct<V>(fn: (v:T)=>V): Tuple2<V,U> {
+        return new Tuple2(fn(this._fst), this._snd);
+    }
+
+    /**
+     * Maps the second component of this tuple to a new value.
+     * Equality requirements.
+     */
+    map2<V>(fn: (v:U)=>V&WithEquality): Tuple2<T,V> {
+        return new Tuple2(this._fst, fn(this._snd));
+    }
+
+    /**
+     * Maps the second component of this tuple to a new value.
+     * No equality requirements.
+     */
+    map2Struct<V>(fn: (v:U)=>V): Tuple2<T,V> {
+        return new Tuple2(this._fst, fn(this._snd));
+    }
+
+    /**
+     * Make a new tuple by mapping both values inside this one.
+     */
+    map<T1,U1>(fn: (a:T,b:U)=> Tuple2<T1,U1>): Tuple2<T1,U1> {
+        return fn(this._fst, this._snd);
     }
 
     /**
--- tests/Tuple2.ts
+++ tests/Tuple2.ts
@@ -12,4 +12,12 @@ describe("Tuple2 manipulation", () => {
         Tuple2.of(1,2).equals(Tuple2.of(1,2))));
     it("equality fails when it should", () => assert.ok(
         !Tuple2.of(1,2).equals(Tuple2.of(2,2))));
+    it("map1 works", () => assert.ok(
+        Tuple2.of(2,2).equals(Tuple2.of(1,2).map1(x=>x*2))));
+    it("map2 works", () => assert.ok(
+        Tuple2.of(1,4).equals(Tuple2.of(1,2).map2(x=>x*2))));
+    it("bimap works", () => assert.ok(
+        Tuple2.of(2,4).equals(Tuple2.of(1,2).map((x,y)=>Tuple2.of(x*2,y*2)))));
+    it("build from array works", () => assert.ok(
+        Tuple2.of(1,2).equals(Tuple2.ofArray([1,2]))));
 });
