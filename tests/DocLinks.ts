f250f85023af81a8ad9a04b76ac820d669fae604
Emmanuel Touzery <etouzery@gmail.com>
2018-11-03 01:26:42 +0100

    now also check user guide links for bad links in a test. just fixed quite a few links thanks to this test.

--- tests/DocLinks.ts
+++ tests/DocLinks.ts
@@ -5,11 +5,23 @@ import { Future } from "../src/Future";
 import * as helpers from "../scripts/make_doc_extra/helpers";
 import * as rp from 'request-promise-native';
 
+function checkUrlsInText(text:string, urlFilter: (url:string)=>boolean): Future<Vector<string>> {
+    const urls = Vector.ofIterable(
+        helpers.requireNotNull(text.match(/https?:.+?(?=[\s\)"'])/g)))
+        .filter(urlFilter);
+    return Future.traverse(urls, url => {
+        console.log(`Checking ${url}...`);
+        return Future.of(rp(url).promise())
+    }, {maxConcurrent:3});
+}
+
 describe("documentation links work", () => {
-    it("README.md", () => {
+    it("README.md", async () => {
         const contents = readFileSync("README.md").toString();
-        const urls = Vector.ofIterable(
-            helpers.requireNotNull(contents.match(/\bhttps?:.+?(?=[\s\)])/g)));
-        return Future.traverse(urls, url =>  Future.of(rp(url).promise()), {maxConcurrent:3});
+        await checkUrlsInText(contents, _=>true);
+    });
+    it("User Guide", async () => {
+        const userGuide = await rp("https://github.com/emmanueltouzery/prelude.ts/wiki/Prelude.ts-user-guide");
+        await checkUrlsInText(userGuide, url => url.indexOf("emmanuel") >= 0 && !url.endsWith(".git"));
     });
 });
